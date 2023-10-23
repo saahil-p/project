3dbd1186df51b56741b6453bdd70ccedc426aa7a
SafwanAhmed08 <110949355+SafwanAhmed08@users.noreply.github.com>
2023-10-23 15:45:58 +0530

    css for payments and newpass

--- src/components/New_pass.js
+++ src/components/New_pass.js
@@ -8,32 +8,36 @@ export default class New_pass extends Component {
     }
   render() {
     return (
+      <>
       <div className='box2'>
-        <h2 style={{textAlign: "center"}}><u>New Pass</u></h2>
-        <form>
-        <div>
-          <p id = "l1">Location : </p>
-          <input type = "text" id = "validity"/>
-          </div>
+        <h1 style={{textAlign: "center",padding:"10px", fontFamily:"TimesNewRoman"}}><u>New Pass</u></h1>
+        <div id = "newpass">
+          <form>
           <div>
-          <p id = "v1">Validity :</p>
-          <select id = "s1">
-            <option value = "1d">1 Day</option>
-            <option value = "1m">1 Month</option>
-            <option value = "1y">1 Year</option>
-          </select>
-          </div> 
-          <div>
-          <p id = "t1">Type : </p>    
-          <select id = "s1">
-            <option value = "1d">2 Wheeler</option>
-            <option value = "1m">4 Wheeler</option>
-            <option value = "1m">2 Wheeler + 4 Wheeler</option>
-          </select>
+            <p id = "l1">Location : </p>
+            <input type = "text" id = "validity"/>
+            </div>
+            <div>
+            <p id = "v1">Validity :</p>
+            <select id = "s1">
+              <option value = "1d">1 Day</option>
+              <option value = "1m">1 Month</option>
+              <option value = "1y">1 Year</option>
+            </select>
+            </div> 
+            <div>
+            <p id = "t1">Type : </p>    
+            <select id = "s1">
+              <option value = "1d">2 Wheeler</option>
+              <option value = "1m">4 Wheeler</option>
+              <option value = "1m">2 Wheeler + 4 Wheeler</option>
+            </select>
+            </div>
+            <button type = "Pay" onClick={this.onPaymentHandler}>Pay</button>
+          </form>
           </div>
-          <button type = "Pay" onClick={this.onPaymentHandler}>Pay</button>
-        </form>
       </div>
+      </>
     )
   }
 }
