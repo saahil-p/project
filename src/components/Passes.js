07118ce9df61ba8142f9ccabcc3dca53e26c0638
SafwanAhmed08 <110949355+SafwanAhmed08@users.noreply.github.com>
2023-10-23 09:51:40 +0530

    added a pass div to make each pass standout on the login page

--- src/components/Passes.js
+++ src/components/Passes.js
@@ -11,22 +11,24 @@ export default class Passes extends Component {
   render() {
     return (
       <>
-      <div className = {this.props.position}>
+      <div className = {this.props.position} >
         <span>
-          <div>
-          <p id = "l1">Location : </p>
-          <p id = "location">{this.props.location}</p>  
-          </div>
-          <div>
-          <p id = "v1">Validity :</p>
-          <p id = "validity"> {this.props.validity}</p> 
-          </div> 
-          <div>
-          <p id = "t1">Type : </p>    
-          <p id = "type">{this.props.type}</p>
-          </div>
-          <div>
-            <button onClick={this.onClickHandler} type = "button" value = "validate" style={{backgroundColor: "orange",marginLeft:"170px",marginBottom: "40px"}}>Validate</button>
+          <div className='pass'>
+            <div>
+            <p id = "l1">Location : </p>
+            <p id = "location">{this.props.location}</p>  
+            </div>
+            <div>
+            <p id = "v1">Validity :</p>
+            <p id = "validity"> {this.props.validity}</p> 
+            </div> 
+            <div>
+            <p id = "t1">Type : </p>    
+            <p id = "type">{this.props.type}</p>
+            </div>
+            <div>
+              <button onClick={this.onClickHandler} type = "button" value = "validate" style={{backgroundColor: "orange",marginLeft:"130px",marginBottom: "40px", }}>Validate</button>
+            </div>
           </div>
         </span>
       </div>
