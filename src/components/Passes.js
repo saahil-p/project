087a1d27cca62223b416ae019f4e68c450dd6af1
saahil-p <148326690+saahil-p@users.noreply.github.com>
2023-10-23 19:19:10 +0530

    added the class pass to pass.js

--- src/components/Passes.js
+++ src/components/Passes.js
@@ -4,32 +4,34 @@ import { Link } from 'react-router-dom';
 import Qr_page from './Qr_page';
 
 export default class Passes extends Component {
-  onClickHandler = ()=>{
-    <Qr_page/> //Figure out why this isnt working.
+  onClickHandler = () => {
+    <Qr_page /> //Figure out why this isnt working.
   }
 
   render() {
     return (
       <>
-      <div className = {this.props.position}>
-        <span>
-          <div>
-          <p id = "l1">Location : </p>
-          <p id = "location">{this.props.location}</p>  
-          </div>
-          <div>
-          <p id = "v1">Validity :</p>
-          <p id = "validity"> {this.props.validity}</p> 
-          </div> 
-          <div>
-          <p id = "t1">Type : </p>    
-          <p id = "type">{this.props.type}</p>
-          </div>
-          <div>
-            <button onClick={this.onClickHandler} type = "button" value = "validate" style={{backgroundColor: "orange",marginLeft:"170px",marginBottom: "40px"}}>{this.props.btntxt}</button>
-          </div>
-        </span>
-      </div>
+        <div className={this.props.position}>
+          <span>
+            <div className='pass'>
+              <div>
+                <p id="l1">Location : </p>
+                <p id="location">{this.props.location}</p>
+              </div>
+              <div>
+                <p id="v1">Validity :</p>
+                <p id="validity"> {this.props.validity}</p>
+              </div>
+              <div>
+                <p id="t1">Type : </p>
+                <p id="type">{this.props.type}</p>
+              </div>
+              <div>
+                <button onClick={this.onClickHandler} type="button" value="validate" style={{ backgroundColor: "orange", marginLeft: "170px", marginBottom: "40px" }}>{this.props.btntxt}</button>
+              </div>
+            </div>
+          </span>
+        </div>
       </>
     )
   }
