b9342a4a90111535a7c41107f4fc57d706b1ab13
SafwanAhmed08 <110949355+SafwanAhmed08@users.noreply.github.com>
2023-10-23 11:56:07 +0530

    completed passes and qr code

--- src/components/Cust_lgin.js
+++ src/components/Cust_lgin.js
@@ -7,20 +7,20 @@ export default class Cust_lgin extends Component {
     return (
         <>
         <div className='box'>
-          <h2 style={{textAlign: "center"}}><u>Active Passes</u></h2>
+          <h1 style={{textAlign: "center",padding:"10px", fontFamily:"TimesNewRoman"}}><u>Active Passes</u></h1>
             <div className = "row">
-              <div className='col'>
+              <div className='col' id ='tl'>
                 <Passes location = "PESU" validity = "Day" type = "2 Wheeler" position = "top-left"/>
               </div>
-              <div className = "col">
+              <div className = "col" id ='tr'>
                 <Passes location = "Mall_1" validity = "Week" type = "4 Wheeler" position = "top-right"/>
               </div>
             </div>
-            <div className='row'>
+            <div className='row' id ='bl'>
               <div className = "col">
               <Passes location = "Lot_32" validity = "Month" type = "2 Wheeler" position = "bottom-left"/>
               </div>
-              <div className='col'>
+              <div className='col' id ='br'>
                 <Passes location = "Office" validity = "Year" type = "4 Wheeler" position = "bottom-right"/>
               </div>
             </div>
